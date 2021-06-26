6eb9141d59ce5241c91fef7cf9e7bf1b0422ef3d
tlastra <tralastra@gmail.com>
2021-06-26 11:42:10 -0700

    corrected apiKey

--- src/script.js
+++ src/script.js
@@ -29,8 +29,8 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-function getForecast(coordinates){
-  let apiKey = “a32a92574243f77d3ebcc51cf8a19a88”;
+function getForecast(coordinates) {
+  let apiKey = "a32a92574243f77d3ebcc51cf8a19a88";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
