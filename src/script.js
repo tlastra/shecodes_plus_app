7ff8bbfe138f3be0eb207e0d963e4e62100dcc06
tlastra <tralastra@gmail.com>
2021-06-26 11:03:52 -0700

    added CSS

--- src/script.js
+++ src/script.js
@@ -30,9 +30,9 @@ function formatDay(timestamp) {
   return days[day];
 }
 function getForecast(coordinates){
-let apiKey = “a32a92574243f77d3ebcc51cf8a19a88”;
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayForecast);
+  let apiKey = “a32a92574243f77d3ebcc51cf8a19a88”;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
@@ -104,7 +104,7 @@ function displayForecast(response) {
     <div class="weather-forecast-date">${day}</div>
     <img src="URL?" alt="" width="40"/>
     <div class="weather-forecast-temperatures">
-    <span class="max">18°</span>
+    <span class="weather-forecast-temperature-max">18°</span>
     <span class="weather-forecast-temperature-min">12°</span>
     </div>
     </div>
