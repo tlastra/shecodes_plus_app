76f9cee5824d79a0d9cf08c7dfdaeb74a7f0d124
tlastra <tralastra@gmail.com>
2021-06-26 12:07:45 -0700

    adding timestamp for forecast

--- src/script.js
+++ src/script.js
@@ -29,6 +29,36 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" alt="" width="40"/>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">${
+        forecastDay.temp.max
+      }°</span>
+      <span class="weather-forecast-temperature-min">${
+        forecastDay.temp.min
+      }°</span>
+    </div>
+    </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function getForecast(coordinates) {
   let apiKey = "a32a92574243f77d3ebcc51cf8a19a88";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
@@ -91,27 +121,6 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
-  let days = ["thur", "fri", "sat", "sun"];
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-    <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="URL?" alt="" width="40"/>
-    <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max">18°</span>
-      <span class="weather-forecast-temperature-min">12°</span>
-    </div>
-    </div>
-    `;
-  });
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
 }
 
 let celsiusTemperature = null;
