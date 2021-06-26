8b2f26d2069576c032741f51af7c295857ea34b0
tlastra <tralastra@gmail.com>
2021-06-26 10:56:56 -0700

    adjusting api

--- src/script.js
+++ src/script.js
@@ -109,8 +109,8 @@ function displayForecast() {
   });
 }
 function getForecast(coordinates){
-Let apiKey= “a32a92574243f77d3ebcc51cf8a19a88”;
-Let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
+let apiKey = “a32a92574243f77d3ebcc51cf8a19a88”;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}& appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 
