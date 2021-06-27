ae80e023b125aa56d5b7040381fca4bbc7e12f45
tlastra <tralastra@gmail.com>
2021-06-27 12:25:28 -0700

    trying to view forecast

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ function formatDay(timestamp) {
 function displayForecast(response) {
   let forecast = response.data.daily;
 
-  let forecastElement = document.querySelector(".forecast");
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
